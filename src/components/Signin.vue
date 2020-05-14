549752d9db0e321df159cd6e3c350ea212b04b2a
Mason France <masonrf1@gmail.com>
2020-05-14 15:05:52 -0600

    Add signin route with skeleton html

---
+++ src/components/Signin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="max-w-sm m-auto my-8">
+    <div class="border p-10 border-grey-light shadow rounded">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
+      <form @submit.prevent="signin">
+        <div class="text-red" v-if="error">
+          {{ error }}
+        </div>
+        <div class="mb-6">
+          <label for="email" class="label">E-mail Address</label>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: '',
-      component: 
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
