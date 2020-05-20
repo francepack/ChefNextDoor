abe57b735d8470f7ed52d336508a5301482c9d3a
Mason France <masonrf1@gmail.com>
2020-05-19 23:48:46 -0600

    Configure tailwind

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,11 +1,16 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    "tailwindcss": "./tailwind.config.js",
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+  // "plugins": {
+  //   "postcss-import": {},
+  //   "postcss-url": {},
+  //   "tailwindcss": tailwindcss,
+  //   "autoprefixer": {}
+  // }
+  "plugins" : [
+    require('postcss-import'),
+    require('tailwindcss'),
+    // require('postcss-url'),
+    require('autoprefixer'),
+  ]
 }
