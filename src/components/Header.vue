013b6a36d3f8abe65584af0976830b530924ff69
Mason France <masonrf1@gmail.com>
2020-05-22 14:20:58 -0600

    Add login scripts to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,3 +5,28 @@
     </a>
   </header>
 </template>
+
+<script>
+export default {
+  name: 'Header',
+  created () {
+    this.signedIn()
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    signedIn () {
+      return localStore.signedIn
+    },
+    signOut () {
+      this.$http.secured.delete('/signin')
+        .then(response => {
+          delete localStorage.csrf
+          delete localStorage.signedInthis.$router.replace('/')
+        })
+        .catch(error => this.setError(error, 'Cannot sign out'))
+    }
+  }
+}
+</script>
