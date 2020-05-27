a6fd52163faf8383738bfdfce0ed959ab7c95c2f
Mason France <masonrf1@gmail.com>
2020-05-26 22:49:56 -0600

    Add Edit and Delete buttons for Vendors

---
+++ src/components/vendors/Vendors.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="text-red-400" v-if="error">{{ error }}</div>
+    <h3 class="font-mono font-regular text-3xl mb-4">Make a vendor profile!</h3>
+    <form @submit.prevent="addVendor">
+      <div class="mb-6">
+        <input type="text" 
+          class="input"
+          autofocus
+          autocomplete="off"
+          placeholder="Type your Vendor Name"
+          v-model="newVendor.name">
+      </div>
+
+      <input type="submit" vlaue="Add Vendor" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green-600 hover:bg-green-700 block w-full py-4 text-white item-center justify-center" />
+    </form>
+
+    <hr class="border border-grey-200 my-6">
+
+    <ul class="list-reset mt-4">
+      <li class="py-4" v-for="vendor in vendors" :key="vendor.id" :vendor="vendor">
+        <div class="flex items-center justify-between flex-wrap">
+          <p class ="block flex-1 font-mono font-semibold flex items-center">
+            {{ vendor.name }}
+          </p>
+          <button class="bg-transparent text-sm hover:bg-blue-600 hover:text-white text-blue-500 border border-blue-400 no-underline font-bold py-2 px-4 mr-2 rounded" @click.prevent="editVendor(vendor)">
+            Edit
+          </button>
+          <button class="bg-transparent text-sm hover:bg-blue-600 hover:text-white text-blue-500 border border-blue-400 no-underline font-bold py-2 px-4 mr-2 rounded" @click.prevent="removeVendor(vendor)">
+            Delete
+          </button>
+        </div>
+
+        <div v-if="artist == editedArtist">
+          <form @submit.prevent="updatedVendor(vendor)">
+            <div>
+              <input />
+              <input />
+            </div>
+          </form>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
