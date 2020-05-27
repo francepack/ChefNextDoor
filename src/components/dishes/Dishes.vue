dbb3e519bf101866f1bf2ffb5c23ac249ce28c3f
Mason France <masonrf1@gmail.com>
2020-05-26 23:42:29 -0600

    Add inputs to Dishes

--- src/components/dishes/Dishes.vue
+++ src/components/dishes/Dishes.vue
@@ -15,6 +15,15 @@
           placeholder="Type a name"
           v-model="newDish.name" />
       </div>
+      <div class="mb-6">
+        <label for="vendor" class="label">Vendor</label>
+        <select id="vendor" class="select" v-model="newDish.vendor">
+          <option disabled value="">Select a vendor</option>
+          <option :value="vendor.id" v-for="vendor in vendors" :key="vendor.id">{{ vendor.name }}></option>
+        </select>
+        <p class="pt-4">Don't see a vendor? <router-link to="/vendors" class="link-gray-500">Create One</router-link></p>
+      </div>
+      <input type="submit" value="Add Dish" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green-500 hover:bg-green-600 block w-full py-4 text-white items-center justify-center" />
     </form>
   </div>
 </template>
